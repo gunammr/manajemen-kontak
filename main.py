d23cbbd384e4ee796813a38aaa122cfbfa2b2721
Guna Januarta <gunajanuarta6@gmail.com>
2024-12-30 21:18:58 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+#Program Manajemen Kontak
+kontak1 = {'nama':"Bunga", 'HP':"08156742", 'email':"bunga@python.com"}
+kontak2 = {'nama':"Jasmine", 'HP':"08512345", 'email':"jasmine@python.com"}
+kontak = [kontak1, kontak2]
+
+while True:
+    print("------------------------")
+    print("Program Manajemen Kontak")
+    print("------------------------")
+    print("1. Melihat semua kontak ")
+    print("2. Menambahkan kontak ")
+    print("3. Menghapus kontak ")
+    print("4. Keluar Program ")
+
+    pilihan = input("Masukkan pilihan anda ( 1 - 4 ) : ")
+
+    if pilihan == '1' : #melihat semua kontak
+        if kontak:
+            print("\nKontak Anda : ")
+            print("--------------")
+            for i, item in enumerate(kontak, start=1):
+                print(f'{i}. {item["nama"]} ({item["HP"]}, {item["email"]})')
+        else :
+            print("Maaf, Kontak Anda Kosong")
+    elif pilihan == '2' : #menambahkan kontak
+        nama = input("Masukkan nama kontak yang baru : ")
+        HP = input("Masukkan nomor HP kontak yang baru : ")
+        email = input("Masukkan email kontak yang baru : ")
+        kontak_baru = {'nama':nama, 'HP':HP, 'email': email}
+        kontak.append(kontak_baru)
+        print("\nKontak Baru Berhasil Ditambahkan")
+
+    elif pilihan == '3' : #menghapus kontak
+        if kontak:
+            print("\nKontak Anda : ")
+            print("--------------")
+            for i, item in enumerate(kontak, start=1):
+                print(f'{i}. {item["nama"]} ({item["HP"]}, {item["email"]})')
+        else:
+            print("Maaf, Kontak Anda Kosong")
+            continue
+        i_hapus = int(input("Masukkan nomor kontak yang akan dihapus : "))
+        del kontak[i_hapus-1]
+        print("\nKontak Berhasil Dihapus")
+
+    elif pilihan == '4' : #keluar program
+        print("Terima Kasih Telah Menggunakan Program Ini ")
+        break
+
+    else :
+        print("Pilihan Anda Salah! Masukkan ulang mulai dari 1 - 4")
